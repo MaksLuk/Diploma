677ffb9cdaf796994fdc0e8e30dc3701b7424f83
Maksim L <ktototamtogda@gmail.com>
2025-03-23 23:42:49 +0300

    chore: данные перенесены на уровень ниже, в App.tsx

--- frontend/src/components/DataComponent.tsx
+++ frontend/src/components/DataComponent.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { UniversityTable } from './UniversityTable.tsx';
 import { LecturersTable } from './LecturersTable.tsx';
 import { ClassroomsTable } from './ClassroomsTable.tsx';
@@ -7,187 +7,24 @@ import { FlowsTable } from './flowsTable.tsx';
 import { Syllabus } from './Syllabus.tsx';
 import { UniversityType, SubjectType, FlowType, SyllabusType } from '../types';
 
-/// В компоненте описаны все элементы на вкладке "Данные"
-export const DataComponent = () => {
-  const [universityData, setUniversityData] = useState<UniversityType[]>([
-    {
-      id: 1,
-      name: "Московский университет",
-      faculties: [
-        {
-          id: 1,
-          name: "Факультет информатики",
-          departments: [
-            {
-              id: 1,
-              name: "Кафедра программирования",
-              specialities: [
-                {
-                  id: 1,
-                  name: "ПИ",
-                  groups: [
-                    {
-                      id: 1,
-                      name: "ПИ-101",
-                      course: "Бакалавриат, 1",
-                      studentsCount: 30
-                    },
-                    {
-                      id: 2,
-                      name: "ПИ-102",
-                      course: "Бакалавриат, 1",
-                      studentsCount: 25
-                    }
-                  ],
-                }
-              ],
-              lecturers: [
-                { id: 1, fullName: "Иванов И.И." },
-                { id: 2, fullName: "Петров П.П." }
-              ],
-              classrooms: [
-                { id: 1, number: "7-205" },
-                { id: 2, number: "7-206" }
-              ]
-            }
-          ],
-          classrooms: [
-            { id: 1, number: "7-101" },
-            { id: 2, number: "7-102" }
-          ]
-        },
-        {
-          id: 2,
-          name: "Факультет математики",
-          departments: [
-            {
-              id: 2,
-              name: "Кафедра алгебры",
-              specialities: [
-                {
-                  id: 1,
-                  name: "АЛГ",
-                  groups: [
-                    {
-                      id: 3,
-                      name: "АЛГ-201",
-                      course: "Магистратура, 1",
-                      studentsCount: 20
-                    },
-                    {
-                      id: 4,
-                      name: "АЛГ-202",
-                      course: "Магистратура, 1",
-                      studentsCount: 22
-                    }
-                  ]
-                }
-              ],
-              lecturers: [
-                { id: 1, fullName: "Сидоров И.И." },
-                { id: 2, fullName: "Смирнов П.П." }
-              ],
-              classrooms: []
-            }
-          ],
-          classrooms: []
-        }
-      ]
-    },
-    {
-      id: 2,
-      name: "Санкт-Петербургский университет",
-      faculties: [
-        {
-          id: 3,
-          name: "Факультет физики",
-          departments: [
-            {
-              id: 3,
-              name: "Кафедра теоретической физики",
-              specialities: [
-                {
-                  id: 1,
-                  name: "ФИЗ",
-                  groups: [
-                    {
-                      id: 5,
-                      name: "ФИЗ-101",
-                      course: "Бакалавриат, 1",
-                      studentsCount: 28
-                    },
-                    {
-                      id: 6,
-                      name: "ФИЗ-102",
-                      course: "Бакалавриат, 1",
-                      studentsCount: 32
-                    }
-                  ]
-                }
-              ],
-              lecturers: [],
-              classrooms: []
-            }
-          ],
-          classrooms: []
-        },
-        {
-          id: 4,
-          name: "Факультет химии",
-          departments: [
-            {
-              id: 4,
-              name: "Кафедра органической химии",
-              specialities: [
-                {
-                  id: 1,
-                  name: "ХИМ",
-                  groups: [
-                    {
-                      id: 7,
-                      name: "ХИМ-201",
-                      course: "Магистратура, 2",
-                      studentsCount: 18
-                    },
-                    {
-                      id: 8,
-                      name: "ХИМ-202",
-                      course: "Магистратура, 2",
-                      studentsCount: 21
-                    }
-                  ]
-                }
-              ],
-              lecturers: [],
-              classrooms: []
-            }
-          ],
-          classrooms: []
-        }
-      ]
-    }
-  ]);
-  const [subjects, setSubjects] = useState<SubjectType[]>([
-    { id: 1, name: "Биология", shortName: "Биология" },
-    { id: 2, name: "Химия", shortName: "Химия" },
-    { id: 3, name: "Физика", shortName: "Физика" },
-    { id: 4, name: "Основны объектно-ориентировааного программирования", shortName: "Основы ООП" },
-  ]);
-  const [flows, setFlows] = useState<FlowType[]>([
-    { id: 1, groups: ["ПИ-101", "ПИ-102"] },
-    { id: 2, groups: ["ПИ-101", "АЛГ-201", "АЛГ-202"] },
-  ]);
-  const [syllabusData, setSyllabusData] = useState<SyllabusType[]>([
-    {
-      id: 1,
-      subject: "Биология",
-      groups: ["ПИ-101", "ПИ-102"],
-      hours: 144,
-      attestation: "Зачёт",
-      lecturer: "Иванов И.И.",
-    }
-  ]);
+export interface DataProps {
+  universityData: UniversityType[];
+  setUniversityData: React.Dispatch<React.SetStateAction<UniversityType[]>>;
+  subjects: SubjectType[];
+  setSubjects: React.Dispatch<React.SetStateAction<SubjectType[]>>;
+  flows: FlowType[];
+  setFlows: React.Dispatch<React.SetStateAction<FlowType[]>>;
+  syllabusData: SyllabusType[];
+  setSyllabusData: React.Dispatch<React.SetStateAction<SyllabusType[]>>;
+}
 
+/// В компоненте описаны все элементы на вкладке "Данные"
+export const DataComponent = ({
+  universityData, setUniversityData,
+  subjects, setSubjects,
+  flows, setFlows,
+  syllabusData, setSyllabusData,
+}: DataProps) => {
   // Получение все названия групп из университетских данных
   const groupNames = useMemo<string[]>(() => {
     const getAllGroups = (data: UniversityType[]): string[] => {
